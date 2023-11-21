05c1eb4333156a5dcf7072fa7a400b34b2e4b820
Bishu Rudra Paul <bishu01mail@gmail.com>
2023-11-21 23:05:39 +0600

    update

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,8 @@
 
 if(screen.width <= 2559){
 
-    
+stepManuActive();
+
 window.addEventListener("load", function(){
     if(document.getElementsByClassName("show")[0]){
         document.querySelector("#right_arrow img").style.transform= "rotate(90deg)";
@@ -20,6 +21,36 @@ Array.from(cardHeader).map(el =>{
         }, 400)
     }
 })
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('introPlayer', {
+    height: '360px',
+    width: '100%',
+    videoId: 'M7lc1UVf-VE',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
 }
 
 // windwo resize functions
@@ -30,20 +61,6 @@ window.onresize = ()=>{
             el.parentElement.removeAttribute("data-bs-toggle");
         })
         
-        // Array.from(document.querySelectorAll(".step")).map(el =>{
-        //     el.classList.add("show")
-        //     el.previousElementSibling.onclick = ()=>{
-        //         Array.from(document.querySelectorAll(".step")).map(el => {
-        //             el.classList.add("show")
-        //         })
-        //         el.classList.remove("show")
-        //     }
-        // })
-        // scroll event action when 2560px screen
-        // document.querySelector(".collapsible_item").onwheel = (evt)=>{
-        //     evt.preventDefault()
-        //     document.querySelector(".collapsible_item").scrollLeft += (String(evt.deltaY).split("")[0] == "-") ? document.querySelector(".collapsible_item").clientWidth : -document.querySelector(".collapsible_item").clientWidth;
-        // }
         document.querySelectorAll(".steps_pills")[0].classList.remove("d-none")
         document.querySelectorAll(".steps_pills")[1].classList.add("d-none")
     }
@@ -59,8 +76,6 @@ window.onresize = ()=>{
     
 }
 
-// document.querySelectorAll(".steps_pills")[0].classList.add("d-none")
-// document.querySelectorAll(".steps_pills")[1].classList.remove("d-none")
 
 if(screen.width >= 2560){
 
@@ -72,48 +87,45 @@ if(screen.width >= 2560){
     //     })
     // }
 
-    // console.log(document.querySelectorAll(`#item${1} .sub_item .collapse`))
+
+    wideStepManuActive()
 
     Array.from(document.querySelectorAll(".step_header")).map(el => {
         el.parentElement.removeAttribute("data-bs-toggle");
     })
 
-    // youtube video play by popup
+
+
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
     var player;
     function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-        width: '100%',
         height: '100%',
-        videoId: 'r7Z08hgGfKc',
+        width: '100%',
+        videoId: 'M7lc1UVf-VE',
         playerVars: {
         'playsinline': 1
         },
         events: {
         'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
         }
     });
     }
 
     // 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
-    // event.target.playVideo();
+        event.target.playVideo();
     }
 
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-    }
-    function stopVideo() {
-    player.stopVideo();
-    }
-    ////////////////////////////////////////////////////////////////
+
     // popup video
     document.getElementById("video_cross").onclick = ()=>{
         player.stopVideo();
@@ -153,7 +165,6 @@ document.querySelector(".navbar-toggler").onclick = ()=>{
 
 
 
-///////////////////////////////////////////////////////////////////////////
 // action take on 2560 equal or upper for popup video
 setTimeout(()=>{
     if(screen.width >= 2560){
@@ -162,53 +173,6 @@ setTimeout(()=>{
 }, 3000)
 
 
-
-// youtube player for intro
-
- // 2. This code loads the IFrame Player API code asynchronously.
- var tag = document.createElement('script');
-
- tag.src = "https://www.youtube.com/iframe_api";
- var firstScriptTag = document.getElementsByTagName('script')[0];
- firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
- // 3. This function creates an <iframe> (and YouTube player)
- //    after the API code downloads.
- var player;
- function onYouTubeIframeAPIReady() {
-   player = new YT.Player('introPlayer', {
-     width: '100%',
-     videoId: 'r7Z08hgGfKc',
-     playerVars: {
-       'playsinline': 1,
-       'controls': 1
-     },
-     events: {
-       'onReady': onPlayerReady,
-       'onStateChange': onPlayerStateChange
-     }
-   });
- }
-
- // 4. The API will call this function when the video player is ready.
- function onPlayerReady(event) {
-//    event.target.playVideo();
-   event.target.setVolume(30);
- }
-
- // 5. The API calls this function when the player's state changes.
- //    The function indicates that when playing a video (state=1),
- //    the player should play for six seconds and then stop.
- var done = false;
- function onPlayerStateChange(event) {
-   if (event.data == YT.PlayerState.PLAYING && !done) {
-     setTimeout(stopVideo, 6000);
-     done = true;
-   }
- }
- function stopVideo() {
-   player.stopVideo();
- }
 
 // horizontal scroll function
 
@@ -295,4 +259,56 @@ function stepBtnToMoveConUpScreen(el, id){
     setTimeout(() => {
             document.getElementById(id).removeAttribute("style")
     }, 2000);
+}
+
+
+// step menu active 
+
+function stepManuActive(){
+    const step = document.querySelectorAll(".collapsible_item > .card");
+    const navLink = document.querySelectorAll(".steps_pills .nav-item > .nav-link");
+
+    window.onscroll= ()=>{
+        step.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop-70;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+            if(top > offset && top < offset+height){
+                navLink.forEach(link => {
+                    link.classList.remove("active")
+                    document.querySelector('[data-step-id*= '+id+']').classList.add('active')
+                })
+            }
+        })
+    }
+    
+    step.forEach(sec => {
+        sec.onclick = ()=>{
+            let secId = sec.getAttribute('id');
+            let stepLink = document.querySelector(`[data-step-id=${secId}]`);
+            navLink.forEach(link => {
+                link.classList.remove('active');
+            })
+            stepLink.classList.add("active");
+        }
+        
+    })
+}
+
+
+function wideStepManuActive(){
+    const step = document.querySelectorAll(".collapsible_item > .card");
+    const navLink = document.querySelectorAll(".steps_pills .nav-item > .nav-link");
+    step.forEach(sec => {
+        sec.onclick = ()=>{
+            let secId = sec.getAttribute('id');
+            let stepLink = document.querySelector(`[data-step-wdId=${secId}]`)
+            navLink.forEach(link => {
+                link.classList.remove('active');
+            })
+            stepLink.classList.add("active");
+        }
+        
+    })
 }
